44d33ebd08c54ee0f89a826abba66b8c1700a1fa
Partha Pratim Das (parthapd) <parthapd@cisco.com>
2019-10-15 15:05:59 +0530

    added travis yml

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t pribhask/multi-client:latest -t pribhask/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t pribhask/multi-server:latest -t pribhask/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t pribhask/multi-worker:latest -t pribhask/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t parthanemo/multi-client:latest -t parthanemo/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t parthanemo/multi-server:latest -t parthanemo/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t parthanemo/multi-worker:latest -t parthanemo/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push pribhask/multi-client:latest
-docker push pribhask/multi-server:latest
-docker push pribhask/multi-worker:latest
+docker push parthanemo/multi-client:latest
+docker push parthanemo/multi-server:latest
+docker push parthanemo/multi-worker:latest
 
-docker push pribhask/multi-client:$SHA
-docker push pribhask/multi-server:$SHA
-docker push pribhask/multi-worker:$SHA
+docker push parthanemo/multi-client:$SHA
+docker push parthanemo/multi-server:$SHA
+docker push parthanemo/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=pribhask/multi-server:$SHA
-kubectl set image deployments/client-deployment client=pribhask/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=pribhask/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=parthanemo/multi-server:$SHA
+kubectl set image deployments/client-deployment client=parthanemo/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=parthanemo/multi-worker:$SHA
